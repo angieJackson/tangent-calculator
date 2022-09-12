dc62f253d90d4f39d324cc38c7ba5dc6d8edec22
Angie Jackson <48957162+angieJackson@users.noreply.github.com>
2022-09-12 13:01:21 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,34 @@
+﻿function milesConverter() {
+    document.getElementById("outputmiles").innerHTML = miles;
+}
+
+function elevation(valNum) {
+    document.getElementById("outputelevation").innerHTML = valNum;
+}
+
+function calculate() {
+    var miles = document.getElementById("miles").value;
+    var feet = miles * 5280;
+    var elevation = document.getElementById("elevation").value;
+
+    var angle = Math.atan(elevation / feet);
+    var degrees = (180 / Math.PI) * angle;
+    let x = degrees.toFixed(4);
+
+    document.getElementById("angle").innerHTML = x;
+
+    var grade = angle * 100;
+    let y = grade.toFixed(1);
+
+    document.getElementById("grade").innerHTML = y;
+}
+
+function resetFunction() {
+    document.getElementById("calcForm").reset();
+
+    const angleInput = document.getElementById("angle");
+    angleInput.value = "";
+
+    const gradeInput = document.getElementById("grade");
+    gradeInput.value = "";
+}
